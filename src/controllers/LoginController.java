d7c8aef2471d891af52b66467f0f08a285d6d160
dev-sharma-sjsu <104236870+dev-sharma-sjsu@users.noreply.github.com>
2022-04-29 18:21:12 -0700

    finished LoginController

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -12,10 +12,10 @@ import javafx.scene.control.TextField;
 import model.User;
 import model.UsersReader;
 
-public class LoginController implements ControllerInterface{
+public class LoginController implements ControllerInterface {
 
 	private CommonObjs appInstance = CommonObjs.getInstance();
-	
+
 	/*
 	 * This method will communicate with the DAO to authenticate the user
 	 */
@@ -25,36 +25,54 @@ public class LoginController implements ControllerInterface{
 	private PasswordField passwordInput;
 	@FXML
 	private Button loginBttn;
+
 	@FXML
 	public void authenticate() {
 		UsersReader ur = new UsersReader();
+		String username = usernameInput.getText();
+		String password = passwordInput.getText();
+
 		try {
-			
-			
-			//do what needs to be done to authenticate
-			//once authenticated set the user for the commonobjs
-			switchScene(loginBttn, "view/main.fxml");
-			
+			ArrayList<User> acc = ur.getAllUser();
+
+			for (User a : acc) {
+
+				if (a.getUsername().equals(username)) {
+					if (a.getPassword().equals(password)) {
+						appInstance.setCurrentUser(a);
+						switchScene(loginBttn, "view/main.fxml");
+					}
+				}
+
+			}
+
+			// do what needs to be done to authenticate
+			// once authenticated set the user for the commonobjs
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
-	 * When the sign up hyperlink is clicked, this method will switch to the sign up page
+	 * When the sign up hyperlink is clicked, this method will switch to the sign up
+	 * page
 	 */
 	@FXML
 	private Hyperlink signUpHl;
+
 	@FXML
 	public void showSignUp() {
 		switchScene(signUpHl, "view/signup.fxml");
 	}
-	
+
 	/*
-	 * When the forgot password hyperlink is clicked, this method will switch to the forgot password page
+	 * When the forgot password hyperlink is clicked, this method will switch to the
+	 * forgot password page
 	 */
 	@FXML
 	private Hyperlink forgotPWHl;
+
 	@FXML
 	public void showForgot() {
 		switchScene(forgotPWHl, "view/forgot.fxml");
